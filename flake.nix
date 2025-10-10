a8bfce9fcfd1ae5fb0b926f9630e80baa3f9bb32
oscilococcinum <gawlikbar@gmail.com>
2025-10-10 12:11:45 +0200

    adds working esp32-stubs

--- flake.nix
+++ flake.nix
@@ -3,44 +3,50 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = { nixpkgs, ... }@inputs: {
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      pkgs = import <nixpkgs> { };
+    in
+    let
+      packageOverrides = pkgs.callPackage ./python-packages.nix { };
+      python = pkgs.python3.override { inherit packageOverrides; };
+      pythonWithPackages = python.withPackages (ps: [ ps.micropython-esp32-stubs ]);
+    in
+    {
+      devShells = builtins.listToAttrs (
+        map (system: {
+          name = system;
+          value =
+            with import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            }; rec {
 
-    devShells = builtins.listToAttrs (map (system: {
-        name = system;
-        value = with import nixpkgs { inherit system; config.allowUnfree = true; }; rec {
+              esp32-dev = pkgs.mkShell {
+                nativeBuildInputs =
+                  with pkgs;
+                  [
+                    micropython
+                    esptool
+                    adafruit-ampy
+                    picocom
+                  ]
+                  ++ (with python3Packages; [
+                    numpy
+                  ])
+                  ++ [
+                    pythonWithPackages
+                    (writeShellScriptBin ''mp-erase'' ''esptool.py --port /dev/ttyUSB0 erase_flash'')
+                    (writeShellScriptBin ''mp-deploy-firmware'' ''esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash 0x1000'')
+                    (writeShellScriptBin ''mp-REPL'' ''picocom /dev/ttyUSB0 -b115200'')
+                    (writeShellScriptBin ''mp-flash'' ''ampy --port /dev/ttyUSB0 put'')
+                  ];
+              };
 
-          esp32-dev = pkgs.mkShell {
-            packages = with pkgs; [
-              micropython
-              esptool
-              adafruit-ampy
-              picocom
-            ] ++ (with python3Packages; [
-              pip
-              numpy
-              python-lsp-server
-            ]) ++ [
-              (writeShellScriptBin ''mp-erase'' ''esptool.py --port /dev/ttyUSB0 erase_flash'')
-              (writeShellScriptBin ''mp-deploy-firmware'' ''esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash 0x1000'')
-              (writeShellScriptBin ''mp-REPL'' ''picocom /dev/ttyUSB0 -b115200'')
-              (writeShellScriptBin ''mp-flash'' ''ampy --port /dev/ttyUSB0 put'')
-              (writeShellScriptBin ''pip-refresh'' ''rm -rf _pip_packages && pip install -r requierments.txt'')
-            ];
-
-            PIP_PREFIX = ''_pip_packages'';
-            PYTHONPATH = ''$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH'';
-            PATH = ''$PIP_PREFIX/bin:$PATH'';
-
-            shellHook = ''
-              unset SOURCE_DATE_EPOCH
-              if [ ! -d "$PIP_PREFIX" ]; then
-               pip install -r requierments.txt
-              fi
-            '';
-          };
-
-          default = esp32-dev;
-        };
-    })[ "x86_64-linux" ]);
-  };
+              default = esp32-dev;
+            };
+        }) [ "x86_64-linux" ]
+      );
+    };
 }
